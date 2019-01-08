7e1de0ea9620e576a0036eaaa7dea8e28a23fb35
Nike477 <46494503+Nike477@users.noreply.github.com>
2019-01-08 21:38:58 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+def Genre():
+    genre = "Rock"
+    return genre
+
+def Artis():
+    artist = "Marilyn Menson"
+    return artist
+
+def Year():
+    year = "2007"
+    return year
+
+genre = Genre()
+artist = Artis()
+year = Year()
+
+print(genre)
+print(artist)
+print(year)
